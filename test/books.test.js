ba784b0782bde623776dcbcc06ef555f99bf2f06
schmartmann <stefanhartmann@gmail.com>
2025-01-27 13:28:30 -0500

    game out actual use case

--- test/books.test.js
+++ test/books.test.js
@@ -10,4 +10,14 @@ describe('GET /books', function() {
             .expect(200)
             .expect(expectedBooks)
     });
+
+    // google docs api
+    // will give structure
+    // that will be saved into DB
+    // also how should this actually work?
+    // 1. write doc in word
+    // 2. "back up" doc in Drive
+    // 3. run script that pulls latest draft from drive
+    // 4. save latest draft into DB
+    // 5. render draft in UI?
 });
