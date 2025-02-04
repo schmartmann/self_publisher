02731b4c8cc903829d39b87087d1cda11f56701a
schmartmann <stefanhartmann@gmail.com>
2025-02-04 12:24:41 -0500

    add single view

--- routes/books.js
+++ routes/books.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const booksRouter = express.Router();
 
+const testBook = require('../test/driveDocFixture.json')
+
 booksRouter.get('/', (req, res) => { 
     res.render('./books/index', {
         title: 'Hey',
         message: 'Hello there!',
         books: [
-            'Braggi & Aneta',
-            'Mecha Comedy'
+            { title: 'Braggi & Aneta', id: 1234 },
+            { title: 'Mecha Comedy', id: 4321 }
         ]
     });
+});
+
+booksRouter.get('/:id', (req, res) => { 
+    res.render('./books/view', { 
+        book: 
+        testBook 
+    })
 });
 
 booksRouter.get('/drive', (req, res) => {
